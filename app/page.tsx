d22a62d745da51cdda87c88458446b0491c930c1
Kaleb-Nim <65989471+Kaleb-Nim@users.noreply.github.com>
2023-10-09 17:32:16 +0800

    Learn alot of react lmaooo

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,60 @@
+"use client";
+
 import Image from 'next/image'
 
+import React, { useEffect, useRef, useState, FormEvent } from "react";
+import { useChat } from "ai/react";
+import Chat from "./components/Chat";
+import {Card} from "./components/Context/Card";
+import {Context} from "./components/Context";
+import { AiFillGithub, AiOutlineInfoCircle } from "react-icons/ai";
+import Header from "./components/header"
+
 export default function Home() {
+
+  const [gotMessages, setGotMessages] = useState(false);
+  const [context, setContext] = useState<string[] | null>(null);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    onFinish: async () => {
+      setGotMessages(true);
+    },
+  });
+
+  const handleMessageSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSubmit(e);
+    setContext(null);
+    setGotMessages(false);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <Header
+      
+      />
+      <Chat
+        input={input}
+        messages={messages}
+        handleInputChanges={handleInputChange}
+        handleMessageSubmit={handleSubmit}
+      />
+        <div className="absolute transform translate-x-full transition-transform duration-500 ease-in-out right-0 w-2/3 h-full bg-gray-700 overflow-y-auto lg:static lg:translate-x-0 lg:w-2/5 lg:mx-2 rounded-lg">
+          <Context className="" selected={context} />
         </div>
-      </div>
+
+        <Card
+          card = {
+            {
+              pageContent: "WUTUTUTUTUTUT",
+              metadata: {
+                hash: "hash"
+              }
+            }
+          }
+          selected = {context}
+        />
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
